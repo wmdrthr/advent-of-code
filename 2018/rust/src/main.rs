9e109d5200e53a4f3494afd1e68fc1c8225a0674
Shantanu Joshi <weemadarthur@yggdrasil.in>
2018-12-02 04:26:24 +0530

    Add Rust skeleton code

---
+++ 2018/rust/src/main.rs
@@ -0,0 +1,51 @@
+extern crate elapsed;
+
+use std::fs;
+use std::env;
+use std::process::exit;
+
+fn get_data(day: u8) -> String {
+    let filename = format!("../inputs/input{:02}.txt", day);
+    let contents = match fs::read_to_string(filename) {
+        Ok(str) => str,
+        Err(_) => {
+            println!("error reading input for day {}", day);
+            String::from("")
+        }
+    };
+
+    contents
+}
+
+fn usage() {
+    println!("usage: adventofcode <day>\n\t1 <= day <= 31");
+}
+
+fn run(day: u8) -> bool {
+    let _data = get_data(day);
+
+    match day {
+        _ => {
+            println!("no solver for day {} yet.", day);
+            false
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        usage();
+        exit(1);
+    } else {
+        let day: u8 = match args[1].trim().parse() {
+            Ok(num) => num,
+            Err(_) => { usage(); exit(2); }
+        };
+
+        let (elapsed, _status) = elapsed::measure_time(|| {
+            run(day);
+        });
+        println!("elapsed = {}", elapsed);
+    }
+}
