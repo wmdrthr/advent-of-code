b58e5eb4ccefa089ecaa33afebb8d6374d44cddf
Shantanu Joshi <weemadarthur@yggdrasil.in>
2019-12-05 04:22:29 +0530

    [RUST] Solution for Day 3
    Crossed Wires

--- 2019/rust/src/adventofcode.rs
+++ 2019/rust/src/adventofcode.rs
@@ -1,43 +1,6 @@
-use std::env;
-use std::fs;
-use std::io::{self, Read};
 
-use chrono::prelude::*;
+pub type V2 = (i32, i32);
 
-const YEAR: i32 = 2019;
-
-pub fn guess_day() -> Result<u8, i32> {
-    let today: Date<Local> = Local::today();
-    if today.year() != YEAR || today.month() != 12 || today.day() > 25 {
-        Err(2)
-    } else {
-        Ok(today.day() as u8)
-    }
-}
-
-pub fn get_data(day: u8) -> String {
-    let mut args: Vec<String> = env::args().collect();
-    if args.len() > 2 {
-        if args[2] == "-" {
-            let mut buffer = String::new();
-            let stdin = io::stdin();
-            let mut handle = stdin.lock();
-
-            handle.read_to_string(&mut buffer)
-                .expect("error reading input data");
-            buffer
-        } else {
-            args.remove(2)
-        }
-    } else {
-        let filename = format!("../inputs/input{:02}.txt", day);
-        let contents = match fs::read_to_string(filename) {
-            Ok(str) => str,
-            Err(_) => {
-                println!("input not found for day {}", day);
-                String::from("")
-            }
-        };
-        contents
-    }
+pub fn manhattan(a: V2, b: V2) -> i32 {
+    (a.0 - b.0).abs() + (a.1 - b.1).abs()
 }
--- 2019/rust/src/main.rs
+++ 2019/rust/src/main.rs
@@ -3,20 +3,64 @@ extern crate chrono;
 
 use std::process::exit;
 use std::env;
+use std::fs;
+use std::io::{self, Read};
+
+use chrono::prelude::*;
+
+const YEAR: i32 = 2019;
 
 pub mod adventofcode;
 pub mod tsiolkovsky;
 pub mod intcode;
+pub mod crossingwires;
 
 fn usage() {
     println!("usage: adventofcode <day>\n\t1 <= day <= 31");
 }
 
+fn guess_day() -> Result<u8, i32> {
+    let today: Date<Local> = Local::today();
+    if today.year() != YEAR || today.month() != 12 || today.day() > 25 {
+        Err(2)
+    } else {
+        Ok(today.day() as u8)
+    }
+}
+
+fn get_data(day: u8) -> String {
+    let mut args: Vec<String> = env::args().collect();
+    if args.len() > 2 {
+        if args[2] == "-" {
+            let mut buffer = String::new();
+            let stdin = io::stdin();
+            let mut handle = stdin.lock();
+
+            handle.read_to_string(&mut buffer)
+                .expect("error reading input data");
+            buffer
+        } else {
+            args.remove(2)
+        }
+    } else {
+        let filename = format!("../inputs/input{:02}.txt", day);
+        let contents = match fs::read_to_string(filename) {
+            Ok(str) => str,
+            Err(_) => {
+                println!("input not found for day {}", day);
+                String::from("")
+            }
+        };
+        contents
+    }
+}
+
 fn run(day: u8, data: String) -> bool {
 
     match day {
-        1 => { tsiolkovsky::solve(data); true }
-        2 => { intcode::solve(data);     true }
+        1 => { tsiolkovsky::solve(data);   true }
+        2 => { intcode::solve(data);       true }
+        3 => { crossingwires::solve(data); true }
         _ => {
             println!("no solver for day {} yet.", day);
             false
@@ -33,13 +77,13 @@ fn main() {
             Err(_) => { usage(); exit(2); }
         }
     } else {
-        match adventofcode::guess_day() {
+        match guess_day() {
             Ok(day) => day,
             Err(code) => { usage(); exit(code); }
         }
     };
 
-    let data = adventofcode::get_data(day);
+    let data = get_data(day);
 
     let (elapsed, _status) = elapsed::measure_time(|| {
         run(day, data);
