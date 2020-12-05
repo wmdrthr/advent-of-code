126d3326afbafacf487339928f7f9402087bd63f
Shantanu Joshi <weemadarthur@yggdrasil.in>
2020-12-05 06:28:34 +0530

    [HASKELL] Solution for Day 4
    Passport Processing

--- 2020/haskell/app/Main.hs
+++ 2020/haskell/app/Main.hs
@@ -12,6 +12,7 @@ import AdventOfCode
 import ReportRepair       (day01)
 import PasswordPhilosophy (day02)
 import TobogganTrajectory (day03)
+import PassportProcessing (day04)
 
 getInput :: Int -> IO String
 getInput day = do
@@ -45,6 +46,7 @@ solve :: Int -> String -> IO ()
 solve 1 = day01 . parseInputNumbers
 solve 2 = day02 . lines
 solve 3 = day03 . lines
+solve 4 = day04
 solve n = error (printf "No solver for day %d yet.\n" n)
 
 main :: IO ()
