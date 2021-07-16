25123e8b973e4383aa9c07cac7f39fee4d5168be
Shantanu Joshi <weemadarthur@yggdrasil.in>
2021-07-16 19:00:37 +0530

    [2016] Python Solutions for Days 6-10

--- 2016/adventofcode.py
+++ 2016/adventofcode.py
@@ -115,25 +115,43 @@ def neighbors(point):
 
 
 
-def display(grid, tiles):
+def display_grid(grid, tileset='.#', dimensions=None):
     # Given a dict representing a point grid, print the grid, using
     # the given tileset.
 
-    max_x = max_y = 0
-    min_x = min_y = 65536
-    for point in grid.keys():
-        min_x = min(min_x, point[0])
-        max_x = max(max_x, point[0])
-        min_y = min(min_y, point[1])
-        max_y = max(max_y, point[1])
+    if dimensions is None:
+        max_x = max_y = 0
+        min_x = min_y = 65536
+        for point in grid.keys():
+            min_x = min(min_x, point[0])
+            max_x = max(max_x, point[0])
+            min_y = min(min_y, point[1])
+            max_y = max(max_y, point[1])
+    else:
+        min_x = min_y = 0
+        max_x, max_y = dimensions
 
-    for y in range(min_y, max_x + 1):
+    for y in range(min_y, max_y):
         row = []
-        for x in range(min_x, max_x + 1):
-            row.append(tiles[grid[(x, y)]])
+        for x in range(min_x, max_x):
+            row.append(tileset[grid[(x, y)]])
         print(''.join(row))
-    print()
 
+
+def occurences(predicate, iterator):
+    # Return number of items in iterator for which the predicate
+    # returns True
+    count = 0
+
+    while True:
+        try:
+            val = next(iterator)
+            if predicate(val):
+                count += 1
+        except StopIterator:
+            pass
+
+    return count
 
 
 ################################################################################
@@ -260,14 +278,12 @@ def solve4(data):
     # Security Through Obscurity
 
     def parse_room(line):
-
         if m := re.match('([a-z-]+)(\d+)\[([a-z]+)\]', line):
             room_id, sector_id, checksum = m.groups()
             room_id = ''.join(room_id.split('-'))
             return room_id, int(sector_id), checksum
 
     def calculate_checksum(room_id):
-
         def cmp2(x, y):
             if x[1] == y[1]:
                 return ord(y[0]) - ord(x[0])
@@ -339,6 +355,219 @@ def solve5(data):
     print()
     yield ''.join(passwords[0])
     yield ''.join(passwords[1])
+
+@with_solutions('ursvoerv', 'vomaypnn')
+def solve6(data):
+
+     # Signals and Noise
+
+    counters = []
+
+    data = data.split('\n')
+    for _ in range(len(data[0])):
+        counters.append(collections.defaultdict(int))
+
+    for line in data:
+        for idx, char in enumerate(line):
+            counters[idx][char] += 1
+
+    messages = [[], []]
+    for counter in counters:
+        counts = list(counter.items())
+        counts.sort(key = lambda v: v[1], reverse = True)
+        messages[0].append(counts[0][0])
+        messages[1].append(counts[-1][0])
+
+    yield ''.join(messages[0])
+    yield ''.join(messages[1])
+
+
+@with_solutions(115, 231)
+def solve7(data):
+
+    # Internet Protocol Version 7
+
+    def is_abba(chars):
+        for idx in range(0, len(chars) - 3):
+            if chars[idx] == chars[idx + 3] and chars[idx + 1] == chars[idx + 2] and chars[idx] != chars[idx + 1]:
+                return True
+        return False
+
+    def get_hypernet_sequences(addr):
+        start = end = 0
+        while True:
+            start = addr.find('[', end)
+            if start == -1:
+                break
+            end = addr.find(']', start)
+            yield addr[start+1:end]
+
+    def supports_tls(addr):
+        for hypernet_sequence in get_hypernet_sequences(addr):
+            if is_abba(hypernet_sequence):
+                return False
+
+        for chars in re.split('\[\w+\]', addr):
+            if is_abba(chars):
+                return True
+
+    def get_area_broadcast_accessors(chars):
+        for idx in range(0, len(chars) - 2):
+            if chars[idx] == chars[idx + 2] and chars[idx] != chars[idx + 1]:
+                yield chars[idx:idx+3]
+        return False
+
+    def byte_allocation_block(area_broadcast_accessor):
+        a = area_broadcast_accessor[0]
+        b = area_broadcast_accessor[1]
+        return f'{b}{a}{b}'
+
+    def supports_ssl(addr):
+        hypernet_sequences = list(get_hypernet_sequences(addr))
+        supernet_sequences = re.split('\[\w+\]', addr)
+
+        for supernet_sequence in supernet_sequences:
+            for aba in get_area_broadcast_accessors(supernet_sequence):
+                # try to find the corresponding BAB in all hypernet
+                # sequences
+                bab = byte_allocation_block(aba)
+                for hypernet_sequence in hypernet_sequences:
+                    if bab in hypernet_sequence:
+                        return True
+
+        return False
+
+
+    tls = ssl = 0
+    for addr in data.split('\n'):
+        if supports_tls(addr):
+            tls += 1
+        if supports_ssl(addr):
+            ssl += 1
+
+    yield tls
+    yield ssl
+
+
+@with_solutions(119, 'ZFHFSFOGPO')
+def solve8(data):
+
+    # Two-Factor Authentication
+    if custom_data:
+        LENGTH, WIDTH = 7, 3
+    else:
+        LENGTH, WIDTH = 50, 6
+
+    instructions = []
+    for line in data.split('\n'):
+        words = line.split(' ')
+        if words[0] == 'rect':
+            x, y = [int(v) for v in words[1].split('x')]
+            instructions.append(('rect', x, y))
+        elif words[0] == 'rotate':
+            a, b = int(words[2][2:]), int(words[-1])
+            instructions.append((words[0], words[1], a, b))
+
+    display = collections.defaultdict(int)
+    for instr in instructions:
+        if instr[0] == 'rect':
+            x, y = instr[1], instr[2]
+            for j in range(y):
+                for i in range(x):
+                    display[(i, j)] = 1
+        elif instr[0] == 'rotate' and instr[1] == 'column':
+            column, step = instr[2], instr[3]
+
+            for _ in range(step):
+                last = display[(column, WIDTH - 1)]
+                for y in range(WIDTH - 1, 0, -1):
+                    display[(column, y)] = display[(column, y - 1)]
+                display[(column, 0)] = last
+        elif instr[0] == 'rotate' and instr[1] == 'row':
+            row, step = instr[2], instr[3]
+
+            for _ in range(step):
+                last = display[(LENGTH - 1, row)]
+                for x in range(LENGTH - 1, 0, -1):
+                    display[(x, row)] = display[(x - 1, row)]
+                display[(0, row)] = last
+
+        if custom_data:
+            print(instr)
+            display_grid(display, dimensions=(LENGTH, WIDTH))
+
+    # Part 1
+    yield sum([v for v in display.values()])
+
+    # Part 2
+    display_grid(display, dimensions=(LENGTH, WIDTH))
+    yield 'ZFHFSFOGPO'
+
+
+@with_solutions(110346, 10774309173)
+def solve9(data):
+
+    # Explosives in Cyberspace
+
+    def decompress(s, recurse=False):
+        if '(' not in s:
+            return len(s)
+
+        count = 0
+        while '(' in s:
+            start = s.find('(')
+            count += start
+            s = s[start:]
+            marker = s[1:s.find(')')]
+            x, y = [int(v) for v in marker.split('x')]
+            s = s[s.find(')') + 1:]
+            if recurse:
+                count += decompress(s[:x], True) * y
+            else:
+                count += len(s[:x]) * y
+            s = s[x:]
+        return count + len(s)
+
+    yield decompress(data)
+    yield decompress(data, True)
+
+
+@with_solutions(157, 1085)
+def solve10(data):
+
+    # Balance Bots
+    if custom_data:
+        CHECK_VALS = (2, 5)
+    else:
+        CHECK_VALS = (17, 61)
+
+    bots = collections.defaultdict(list)
+    outputs = collections.defaultdict(list)
+    instructions = {}
+    targets = {'bot': bots, 'output': outputs}
+
+    for line in data.split('\n'):
+        if line.startswith('value'):
+            num, bot = [int(v) for v in re.findall(r'\d+', line)]
+            bots[bot].append(num)
+        elif line.startswith('bot'):
+            bot, low, high = [int(v) for v in re.findall(r'\d+', line)]
+            trgts = re.findall(r' (bot|output)', line)
+            instructions[bot] = (trgts, low, high)
+
+    while len(bots) > 0:
+        for bot, vals in dict(bots).items():
+            if len(vals) == 2:
+                vals = sorted(bots.pop(bot))
+                if tuple(vals) == CHECK_VALS:
+                    yield bot
+                (trgts, low, high) = instructions[bot]
+                targets[trgts[0]][low].append(vals[0])
+                targets[trgts[1]][high].append(vals[1])
+
+    a, b, c = [outputs[idx][0] for idx in range(3)]
+    yield a * b * c
+
 
 ################################################################################
 
