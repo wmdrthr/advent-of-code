c82d3deed8f39bb0065cca4e05ad60dee72f2683
Shantanu Joshi <weemadarthur@yggdrasil.in>
2021-12-03 13:20:31 +0530

    [HASKELL] Solution for Day 2
    Dive!

--- 2021/haskell/test/SonarSweepSpec.hs
+++ 2021/haskell/test/SonarSweepSpec.hs
@@ -6,7 +6,5 @@ import SonarSweep as SUT
 spec :: Spec
 spec = do
   let testInput = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-  context "Part 1" $ do
-    it "increasing values => 7" $ do SUT.calculate testInput `shouldBe` 7
-  context "Part 2" $ do
-    it "sliding window increasing values => 5" $ do SUT.calculate (SUT.sums testInput) `shouldBe` 5
+  it "Part 1: increasing values => 7" $ do SUT.calculate testInput `shouldBe` 7
+  it "Part 2: sliding window increasing values => 5" $ do SUT.calculate (SUT.sums testInput) `shouldBe` 5
