1fd8d86c2b2c003734ca3948e59385cef0503f19
Shantanu Joshi <weemadarthur@yggdrasil.in>
2021-12-06 18:34:05 +0530

    [RUST] Solution for Day 4
    Giant Squid

--- 2021/rust/src/main.rs
+++ 2021/rust/src/main.rs
@@ -13,6 +13,7 @@ use chrono::prelude::*;
 pub mod sonarsweep;
 pub mod dive;
 pub mod binarydiagnostic;
+pub mod giantsquid;
 
 fn usage() {
     println!("usage: adventofcode <day> [-|input file]");
@@ -64,6 +65,7 @@ fn run(day: u8, data: String) {
         1  => { sonarsweep::solve(data);                    }
         2  => { dive::solve(data);                          }
         3  => { binarydiagnostic::solve(data);              }
+        4  => { giantsquid::solve(data);                    }
         _  => { println!("no solver for day {} yet.", day); }
     }
 }
