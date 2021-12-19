41703f4e9bb4996f293ddf5a1a57ab3051fa5d81
Shantanu Joshi <weemadarthur@yggdrasil.in>
2021-12-19 20:33:07 +0530

    [PYTHON] Solution for Day 18
    Snailfish

--- 2021/adventofcode.py
+++ 2021/adventofcode.py
@@ -12,6 +12,8 @@ import itertools
 import collections
 import statistics
 import math
+import functools
+import json
 
 import pytz
 import requests
@@ -237,6 +239,8 @@ def test():
                     color = '\033[1;31m' # red
                 elif elapsed_ms > 100:
                     color = '\033[1;33m' # yellow
+                elif timing < (1000 * 1000):
+                    color = '\033[1;34m' # blue
                 else:
                     color = '\033[1;32m' # green
                 print(f'{color}{format_elapsed_time(timing):>10}\033[0m')
@@ -1007,6 +1011,75 @@ def solve17(data):
     yield sum(simulate(vx, vy) for vx in range(tx[1] + 1) for vy in range(ty[0], -ty[0]))
 
 
+
+@with_solutions(4120, 4725)
+def solve18(data):
+
+    # Snailfish
+
+    def add_left(x, n):
+        if n is None:
+            return x
+        if isinstance(x, int):
+            return x + n
+        return [add_left(x[0], n), x[1]]
+
+
+    def add_right(x, n):
+        if n is None:
+            return x
+        if isinstance(x, int):
+            return x + n
+        return [x[0], add_right(x[1], n)]
+
+
+    def explode(x, n = 4):
+        if isinstance(x, int):
+            return False, None, x, None
+        if n == 0:
+            return True, x[0], 0, x[1]
+        a, b = x
+        exp, left, a, right = explode(a, n - 1)
+        if exp:
+            return True, left, [a, add_left(b, right)], None
+        exp, left, b, right = explode(b, n - 1)
+        if exp:
+            return True, None, [add_right(a, left), b], right
+        return False, None, x, None
+
+    def split(x):
+        if isinstance(x, int):
+            if x >= 10:
+                return True, [x // 2, math.ceil(x / 2)]
+            return False, x
+        a, b = x
+        change, a = split(a)
+        if change:
+            return True, [a, b]
+        change, b = split(b)
+        return change, [a, b]
+
+    def add(a, b):
+        x = [a, b]
+        while True:
+            change, _, x, _ = explode(x)
+            if change:
+                continue
+            change, x = split(x)
+            if not change:
+                break
+        return x
+
+    def magnitude(x):
+        if isinstance(x, int):
+            return x
+        return 3 * magnitude(x[0]) + 2 * magnitude(x[1])
+
+
+    numbers = list(map(json.loads, data.splitlines()))
+    yield magnitude(functools.reduce(add, numbers))
+
+    yield max(magnitude(add(a, b)) for a, b in itertools.permutations(numbers, 2))
 
 ################################################################################
 
